764b0ba86246cea160eebe26ffa359dfcb0195d2
hudangrh <hudangrhkuliah@gmail.com>
2026-05-15 15:09:17 +0700

    default

--- stack.cpp
+++ stack.cpp
@@ -107,6 +107,9 @@ int main()
         case 4:
             cout << "Exiting program." << endl;
             break;
+        default:
+            cout << "invalid choice.try agai." << endl;
+            break;
         }
     }
 }
